c7c9ca9cfca51403899008e26f395ccb98a31424
Sebastian Cardozo <cardozosebastian106@gmail.com>
2025-09-02 22:31:37 -0300

    v1 animaciones

--- Assets/Script/Jugador.cs
+++ Assets/Script/Jugador.cs
@@ -14,7 +14,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Transform TocandoPiso;//detecta el piso
     public float RadioDePiso = 0.1f;//area de deteccion del piso
     public LayerMask CapaDePiso;//nose aun
-    private Animator animator;// controla las animaciones
+    public Animator animator;// controla las animaciones
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>(); // busca el componente y lo guarda aca 
@@ -27,17 +27,23 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         movimiento = Input.GetAxisRaw("Horizontal");// Lee la entrada horizontal del jugador (-1 = izquierda, 0 = sin movimiento, 1 = derecha)
         rb2d.linearVelocity = new Vector2(movimiento * velocidad, rb2d.linearVelocity.y);//le digo que mantenga una velocidad depenediendo la emtrada del jugador sin afectar al salto
+        // Si movimiento es distinto de 0, corre. Si es 0, está quieto
+        animator.SetBool("correr", movimiento != 0);
 
         // Voltea el sprite automáticamente
         if (movimiento != 0)
         {
             spriteRenderer.flipX = (movimiento < 0);//si el personaje se mueve en el eje x para numeros negativos rota
+            animator.SetBool("salto", false);
+            
         }
         if (Input.GetButton("Jump") && esPiso)//si se preciona el espacio y detecta el piso
         {
             rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, salto);
+            animator.SetBool("correr", false);
+            animator.SetBool("salto", true);
         }
-        animator.SetFloat("velocidad", Mathf.Abs(movimiento));
+       
         }
     private void FixedUpdate()//se analiza a cada rato 
     {
